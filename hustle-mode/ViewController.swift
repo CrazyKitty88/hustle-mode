258d84f3e18df50bb26be10d43fe86f1d0af72ff
Grant Wenzelow <CrazyKitty@Grants-MacBook-Pro.local>
2019-04-01 17:47:58 -0500

    initial commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Grant Wenzelow on 4/1/19.
+//  Copyright © 2019 Grant Wenzelow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
